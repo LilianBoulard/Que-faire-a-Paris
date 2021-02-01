7511f384ba982296fb5e507212762efc12f55c75
Lilian <lilian@boulard.fr>
2021-02-01 16:46:39 +0100

    Add JSON parsing functions

--- qfap/utils.py
+++ qfap/utils.py
@@ -1,4 +1,26 @@
+import json
+
 import dateutil.parser as dp
+
+
+def encode_json(dictionary: dict) -> str:
+    """
+    Takes a dictionary and returns a JSON-encoded string.
+
+    :param dict dictionary: A dictionary.
+    :return str: A JSON-encoded string.
+    """
+    return json.JSONEncoder().encode(dictionary)
+
+
+def decode_json(json_string: str) -> dict:
+    """
+    Takes a message as a JSON string and unpacks it to get a dictionary.
+
+    :param str json_string: A message, as a JSON string.
+    :return dict: An unverified dictionary. Do not trust this data.
+    """
+    return json.JSONDecoder().decode(json_string)
 
 
 def convert_iso8601_to_timestamp(iso: str) -> int:
